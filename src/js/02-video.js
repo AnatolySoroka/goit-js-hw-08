9c22c97439afa62a47901b0e56b5f871a8eba54d
AnatolySoroka <toliksoroka66@gmail.com>
2023-02-28 12:47:54 +0200

    task-02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(timeupdate, 1000));
+
+function timeupdate(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+const currentTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
